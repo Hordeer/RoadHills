27b8c85e2b187b98d4367b49e71dc647609fae54
Alexandr Janashvili <alexandrjanashvili@gmail.com>
2021-01-30 08:28:17 +0400

    fix logging

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     include_once("index.html");
 
     $file = fopen("ip.txt","a");
-    $ip=$_SERVER['REMOTE_ADDR']."\n";
+    $ip=$_SERVER['REMOTE_ADDR']." | ".$_SERVER['HTTP_USER_AGENT']."\n";
     echo fwrite($file,$ip);
     fclose($file);
 ?>
