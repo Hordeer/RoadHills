5f59361df7fa93da84cb6fa62b1f64840c961a22
Alexandr Janashvili <alexandrjanashvili@gmail.com>
2021-01-31 11:18:47 +0400

    remove stat

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
     include_once("index.html");
-
-    $file = fopen("bb435712f3023758619f2ae60c2cd94a.txt","a");
-    $ip=$_SERVER['REMOTE_ADDR']." | ".$_SERVER['HTTP_USER_AGENT']."\n";
-    echo fwrite($file,$ip);
-    fclose($file);
 ?>
